f65eb62cd191191355379006e740ec7b22edc614
jdh1999 <jackhettinger@comcast.net>
2020-02-17 13:26:15 -0500

    push

---
+++ js/overlay.js
@@ -0,0 +1,9 @@
+//File to activate the overlay effect or to turn it off
+
+function on() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+}
